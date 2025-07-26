d6f4d533a36361e1710ebbfc177446942e8ceaab
Prachi Adhwaryu <prachiadhwaryu@gmail.com>
2025-07-25 20:05:56 -0400

    Upgrade Spring boot starter version.

--- src/main/java/com/foodrecall/config/WebConfig.java
+++ src/main/java/com/foodrecall/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class WebConfig {
@@ -12,7 +13,7 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
